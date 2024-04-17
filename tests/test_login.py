e9b653784cc43254ad374f89111fd19c1f015ea3
Vlad Guliakin <vladislav.guliakin@gmail.com>
2024-04-16 22:34:33 -0400

    Created tests of login with empty password and username data

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,14 +1,33 @@
 from src.pages.login_page import LoginPage
 from src.utilitis.urls import Urls
+from src.locators.login_locators import LoginLocators
+from src.data.auth_data import AuthData
 
 
 class TestLogin():
+    url = Urls()
 
-    url = Urls.base_url
-
-    def test_login(self, browser):
-
-        page = LoginPage(browser, self.url)
+    def test_login_with_valid_credentials(self, browser):
+        page = LoginPage(browser, self.url.base_url)
 
         page.open()
         page.login()
+
+        assert page.get_current_url() == self.url.main_page_url
+        assert page.verify_login_success()
+
+    def test_login_with_empty_username(self, browser):
+        page = LoginPage(browser, self.url.base_url)
+
+        page.open()
+
+        assert page.verify_login_with_empty_username()
+
+    def test_login_with_empty_password(self, browser):
+        page = LoginPage(browser, self.url.base_url)
+
+        page.open()
+
+        assert page.verify_login_with_empty_password()
+
+
